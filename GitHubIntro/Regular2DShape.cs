2fe7acb052491c0407d7e709137b727797b1721c
Evanie Elliot <Evaniee7@outlook.com>
2023-08-31 12:22:35 +0100

    Added abstract 2D shape

---
+++ GitHubIntro/Regular2DShape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubIntro
+{
+    public abstract class Regular2DShape
+    {
+        protected double _length;        // Length of a single side
+
+        /// <summary>
+        /// Length of a single side
+        /// </summary>
+        public double Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+
+        /// <summary>
+        /// A regular 2D shape, all sides are the same length
+        /// </summary>
+        /// <param name="length">Length of a single side</param>
+        public Regular2DShape(double length) { }
+
+        /// <summary>
+        /// Get the Area of the shape
+        /// </summary>
+        /// <returns>Area of the shape</returns>
+        public abstract double GetArea();
+    }
+}
